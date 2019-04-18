22305213f88a204521f4bf7aaadf365b93465037
xingyuliuNLP <xingyuliufrance@gmail.com>
2019-04-18 18:11:17 +0200

    separate ST snippets

--- temp.py
+++ temp.py
@@ -1,10 +1,9 @@
 ## edit sumblime text part > export all snippets in multiple files named as prefix.sublime-snippet
 
 # locate the beginning and the end of every snippet <snippet></snippet> and mark their line numbers
-dictionary???????
+
 # read the last but one line in every snippet
 
 # the content between tags is prefix
 
 # export all lines between every beginning and end
-  
